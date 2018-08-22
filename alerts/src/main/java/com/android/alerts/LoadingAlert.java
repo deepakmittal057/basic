ba3c2f6321c153b19e095f255149defd8fd29342
deepak mittal <depak330@gmail.com>
2018-08-22 14:53:19 +0530

    check

---
+++ alerts/src/main/java/com/android/alerts/LoadingAlert.java
@@ -0,0 +1,77 @@
+package com.android.alerts;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+
+/**
+ * Created by amit.singh on 13-Dec-17.
+ */
+
+public class LoadingAlert {
+
+    PopupWindow popupWindow;
+    View view;
+    TextView textTv;
+
+    public LoadingAlert(Context context) {
+        //activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = layoutInflater.inflate(R.layout.loading_alert, null, false);
+        textTv=(TextView)view.findViewById(R.id.textTv);
+
+
+        Display display = ((Activity)context).getWindowManager().getDefaultDisplay();
+        final Point size = new Point();
+        display.getSize(size);
+
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int width = displayMetrics.widthPixels;
+        int hight = displayMetrics.heightPixels;
+        popupWindow = new PopupWindow(view, width, hight, true);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setFocusable(true);
+        alertHide();
+    }
+
+
+
+    public void alertShow(String s){
+        try {
+            if(!popupWindow.isShowing()){
+                try {
+                    textTv.setText(s);
+                    popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void alertHide(){
+        try {
+            if(popupWindow.isShowing()){
+                try {
+                    popupWindow.dismiss();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
